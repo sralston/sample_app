180aeaa97259c1a313761a167cd442d037c39e29
sralston <sralston@gmail.com>
2011-05-10 01:53:07 -0700

    done with this chapt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update, :index]
+  before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user,   :only => :destroy
+
+  def destroy
+  	User.find(params[:id]).destroy
+  	flash[:success] = "User destroyed"
+  	redirect_to users_path
+  end
+
+  def index
+  	@title = "All Users"
+  	@users = User.paginate(:page => params[:page])
+  end
 
   def new
   	@user = User.new
@@ -20,7 +34,39 @@ class UsersController < ApplicationController
   		@title = "Sign up"
   		render 'new'
   	end
-  
   end
+  
+  def edit
+  	@title = "Edit User"
+  end
+  
+  def update
+  	#@user = User.find(params[:id])
+  	if @user.update_attributes(params[:user])
+  		flash[:success] = "Profile updated!"
+  		redirect_to @user
+  	else
+  		@title = "Edit user"
+  		render 'edit'
+  	end
+  end
+  
+  private
+  	
+  	def authenticate
+  		deny_access unless signed_in?
+  	end
+  	
+  	def correct_user
+  		@user = User.find(params[:id])
+  		if !current_user?(@user)
+	  		flash[:notice] = "Why are you trying to edit someone other than you, twit?"
+	  		redirect_to(root_path)
+  		end
+  	end
+  	
+  	def admin_user
+  		redirect_to(root_path) unless current_user.admin?
+  	end
 
 end
